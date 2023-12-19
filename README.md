4807bffd6b065243a65ba5afb7113264f3f56bf1
EC2 Default User <ec2-user@ip-172-31-41-124.ec2.internal>
2023-12-19 19:14:03 +0000

    should define your key file

---
+++ README.md
@@ -0,0 +1,17 @@
+Terraform Module to provision an AWS EC2 instance with the latest amazon linux 2023 ami and installed docker in it.
+
+Not intended for production use. It is an example module.
+
+It is just for showing how to create a publish module in Terraform Registry.
+
+- Usage
+```hcl
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "docker_instance" {
+    source = "raifehizlan/docker-instance/aws"
+    key_name = "clarusway"
+}
+```
---
+++ main.tf
@@ -0,0 +1,66 @@
+data "aws_ami" "amazon-linux-2023" {
+  owners      = ["amazon"]
+  most_recent = true
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "owner-alias"
+    values = ["amazon"]
+  }
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023*"]
+  }
+}
+
+resource "aws_instance" "tfmyec2" {
+  ami = data.aws_ami.amazon-linux-2023.id
+  instance_type = var.instance_type
+  count = var.num_of_instance
+  key_name = var.key_name
+  vpc_security_group_ids = [aws_security_group.tf-sec-gr.id]
+  user_data = templatefile("${abspath(path.module)}/userdata.sh", {myserver = var.server-name})
+  tags = {
+    Name = var.tag
+  }
+}
+
+resource "aws_security_group" "tf-sec-gr" {
+  name = "${var.tag}-terraform-sec-grp"
+  tags = {
+    Name = var.tag
+  }
+
+  dynamic "ingress" {
+    for_each = var.docker-instance-ports
+    iterator = port
+    content {
+      from_port = port.value
+      to_port = port.value
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  egress {
+    from_port =0
+    protocol = "-1"
+    to_port =0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
